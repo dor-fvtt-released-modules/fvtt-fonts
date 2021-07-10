5d03ce5e9af05606dfa8fa855a34f6ba5f862461
James Standley <doresain@gmail.com>
2021-07-09 18:22:59 -0700

    Setup node, eslint, prettier.

--- README.md
+++ README.md
@@ -1,19 +1,19 @@
 ## Ideas:
 
-* Toggle to auto-include dungeondraft fonts
-* Toggle(s) to auto-include pack(s) of themed fonts (fantasy, sci-fi, etc) from Google Fonts
-* Include free font file(s)?
-* User font directory? 
+- Toggle to auto-include dungeondraft fonts
+- Toggle(s) to auto-include pack(s) of themed fonts (fantasy, sci-fi, etc) from Google Fonts
+- Include free font file(s)?
+- User font directory?
 
 ## To Do:
-* Include fonts with download
-* Account for Polyglot
-* Account for Forien's Custom Fonts
-* Add game system fonts for popular systems (https://www.reddit.com/r/Pathfinder2e/comments/mo2onb/text_fonts_used_in_the_books/)
-* Handle adding duplicates (within and across categories)
-* Move fonts on ManageFonts into sections
-* Move code out of fvtt-fonts class into add-fonts controller
-* Finish wiring up Add Fonts
-* Build install/uninstall workflow
-* Polish manage fonts UI, change opening location
-* Add dungeondraft fonts/settings/etc
+
+- Include fonts with download
+- Account for Polyglot
+- Account for Forien's Custom Fonts
+- Add game system fonts for popular systems (https://www.reddit.com/r/Pathfinder2e/comments/mo2onb/text_fonts_used_in_the_books/)
+- Handle adding duplicates (within and across categories)
+- Move fonts on ManageFonts into sections
+- Move code out of fvtt-fonts class into add-fonts controller
+- Finish wiring up Add Fonts
+- Build install/uninstall workflow
+- Polish manage fonts UI, change opening location
--- module/utils.js
+++ module/utils.js
@@ -1,72 +1,89 @@
-import constants from './constants.js';
+import constants from "./constants.js";
 
 // Simplifies localization
-export function loc (domain,section,key,locPlaceholders={}) {
-	return game.i18n.format(`${constants.moduleName}.${domain}.${section}.${key}`,locPlaceholders);
+export function loc(domain, section, key, locPlaceholders = {}) {
+  return game.i18n.format(
+    `${constants.moduleName}.${domain}.${section}.${key}`,
+    locPlaceholders
+  );
 }
 
 // Simplifies logging to the console
-export function log (logString,type='info') {
-	if (type === 'error') {
-		console.error(`${constants.moduleTitle} | ${logString}`);
-	} else if (type === 'warn') {
-		console.warn(`${constants.moduleTitle} | ${logString}`);
-	} else if (type === 'debug') {
-		console.warn(`DEBUG | ${constants.moduleTitle} | ${logString}`);
-	} else {
-		console.log(`${constants.moduleTitle} | ${logString}`);
-	}
+export function log(logString, type = "info") {
+  if (type === "error") {
+    console.error(`${constants.moduleTitle} | ${logString}`);
+  } else if (type === "warn") {
+    console.warn(`${constants.moduleTitle} | ${logString}`);
+  } else if (type === "debug") {
+    console.warn(`DEBUG | ${constants.moduleTitle} | ${logString}`);
+  } else {
+    console.log(`${constants.moduleTitle} | ${logString}`);
+  }
 }
 
 // Simplifies showing a notification
-export function notify (locMap={},type='info',locPlaceholders,options = {'permanent':false,'localize':true}) {
-	const locString = loc(locMap.locDomain,locMap.locSection,locMap.locKey,locPlaceholders)
-	ui.notifications.notify(`${constants.moduleTitle} | ${locString}`,type,{'permanent':options.permanent,'localize':options.localize});
-	log(locString,type)
+export function notify(
+  locMap = {},
+  type = "info",
+  locPlaceholders,
+  options = { permanent: false, localize: true }
+) {
+  const locString = loc(
+    locMap.locDomain,
+    locMap.locSection,
+    locMap.locKey,
+    locPlaceholders
+  );
+  ui.notifications.notify(`${constants.moduleTitle} | ${locString}`, type, {
+    permanent: options.permanent,
+    localize: options.localize,
+  });
+  log(locString, type);
 }
 
 // Simplified debug logging
-export function debug(debugString,force = false) {
-	if (game.modules.get('_dev-mode')) {
-		try {
-			const isDebugging = game.modules.get('_dev-mode')?.api?.getPackageDebugValue(constants.moduleName);
+export function debug(debugString, force = false) {
+  if (game.modules.get("_dev-mode")) {
+    try {
+      const isDebugging = game.modules
+        .get("_dev-mode")
+        ?.api?.getPackageDebugValue(constants.moduleName);
 
-			if (force || isDebugging) {
-				log(debugString, 'debug');
-			}
-		} catch (e) {
-		}
-	}
+      if (force || isDebugging) {
+        log(debugString, "debug");
+      }
+    } catch (e) {}
+  }
 }
 
 // Determines if string is false or empty
-export function isEmpty (string) {
-	return !string || string.length === 0 || /^\s*$/.test(string);
+export function isEmpty(string) {
+  return !string || string.length === 0 || /^\s*$/.test(string);
 }
 
 // Checks if something is an object
-export function isObject (variable) {
-	return Object.prototype.toString.call(variable) === '[object Object]'
+export function isObject(variable) {
+  return Object.prototype.toString.call(variable) === "[object Object]";
 }
 
 // Simplifies setting settings
- export function settingSet (key, value) {
-	game.settings.set(constants.moduleName, key, value);
+export function settingSet(key, value) {
+  game.settings.set(constants.moduleName, key, value);
 }
 
 // Simplifies getting settings
-export function settingGet (key) {
-	return game.settings.get(constants.moduleName, key);
+export function settingGet(key) {
+  return game.settings.get(constants.moduleName, key);
 }
 
 // Registers useful handlebars helper functions
 export function setupHandlebarsHelpers() {
-	Handlebars.registerHelper('concat', function(...params) {
-		// Ignore the object appended by handlebars.
-		if (isObject(params[params.length - 1])) {
-			params.pop();
-		}
+  Handlebars.registerHelper("concat", function (...params) {
+    // Ignore the object appended by handlebars.
+    if (isObject(params[params.length - 1])) {
+      params.pop();
+    }
 
-		return params.join('');
-	})
+    return params.join("");
+  });
 }
